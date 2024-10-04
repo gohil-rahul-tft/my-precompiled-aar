52a4e09c5229eaf63867557b05fcf68dd33c968d
rahul-admin <gohil.rahul@tftus.com>
2024-10-04 23:54:53 +0530

    Initial commit

---
+++ build.gradle.kts
@@ -0,0 +1,31 @@
+plugins {
+    `maven-publish`
+}
+
+publishing {
+    publications {
+        create<MavenPublication>("flutterModuleAAR") {
+            groupId = "com.example.my_flutter_module"
+            artifactId = "flutter_release"
+            version = "1.0"
+
+            // Specify the AAR and POM file paths
+            artifact(file("$rootDir/libs/com/example/my_flutter_module/flutter_release/1.0/flutter_release-1.0.aar")) {
+                extension = "aar"
+            }
+
+            pom {
+                name.set("Flutter Module AAR")
+                description.set("A Flutter module packaged as an AAR")
+//                url.set("https://github.com/your-username/flutter_module_repo")
+            }
+        }
+    }
+
+    repositories {
+        maven {
+            // You can publish to the local repo for testing first
+            url = uri("${layout.buildDirectory}/repo")
+        }
+    }
+}
